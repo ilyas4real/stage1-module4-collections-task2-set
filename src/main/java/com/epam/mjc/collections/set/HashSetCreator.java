558568c28bec7ebce489e3daf25fbbbaf4b0a8c1
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-03-24 21:12:44 +0100

    finished

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i : sourceList) {
+            if (i%2==0) {
+                hashSet.add(i);
+                while (i%2==0) {
+                    hashSet.add(i/2);
+                    i /= 2;
+                }
+            } else {
+                hashSet.add(i);
+                hashSet.add(i*2);
+            }
+        }
+        return hashSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,18 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> set = new HashSet<>();
+        for (String s : firstSet) {
+            if (secondSet.contains(s) && !thirdSet.contains(s)) {
+                set.add(s);
+            }
+        }
+
+        for (String s : thirdSet) {
+            if (!firstSet.contains(s) && !secondSet.contains(s)) {
+                set.add(s);
+            }
+        }
+        return set;
     }
 }
